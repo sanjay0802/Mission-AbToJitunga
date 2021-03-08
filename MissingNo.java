32c7103b04e32ca1599056fe725add5c4e8c0325
sanjay0802 <sanjayyadav0802@gmail.com>
2021-03-08 23:57:20 +0530

    Array Sum

---
+++ MissingNo.java
@@ -0,0 +1,27 @@
+package com.logic.abtojitunga.array;
+
+public class MissingNo {
+
+	
+	
+	public static void main(String[] args) {
+		int n=100;
+		int missNo=7;
+		
+		
+		int sum=0;
+		for(int i=0;i<=100;i++) {
+		
+		      
+		    sum=sum+i;
+		
+		
+	}
+		
+		System.out.println("Origanl Sum:"+sum);
+		
+		
+	
+		
+}
+}
